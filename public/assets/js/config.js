a417fc528682430b0dda2a20b35beaeb887cfd02
BTCforPlebs <btcforplebs@gmail.com>
2025-09-27 00:33:28 -0400

    non

--- public/assets/js/config.js
+++
@@ -1,7 +0,0 @@
-// Configuration for the frontend
-const config = {
-    // Use services.btcforplebs.com in production, localhost in development
-    apiBaseUrl: window.location.hostname === 'localhost' 
-        ? 'http://localhost:5252' 
-        : window.location.protocol + '//services.btcforplebs.com'
-};
